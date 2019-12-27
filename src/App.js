82ab0af1e403bd2b84b75441a85551b5129f3195
karn21 <karn212000@gmail.com>
2019-12-27 13:46:34 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,72 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
+    }
+  }
+
+  CountIncreaseHandler = () => (
+    this.setState(
+      {
+        count: this.state.count + 1,
+      }
+    )
+  )
+
+  CountDecreaseHandler() {
+    if (this.state.count !== 0) {
+      this.setState(
+        {
+          count: this.state.count - 1,
+        }
+      )
+    }
+
+  }
+
+
+  CountResetHandler = () => (
+    this.setState(
+      {
+        count: 0,
+      }
+    )
+  )
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>
+            <i className="fa fa-play-circle" aria-hidden="true"></i>
+            &nbsp;
+            Counter
+        </h1>
+        </header>
+
+        <div className="counter">
+          <div className="count">
+            {this.state.count}
+          </div>
+          <div>
+            <button onClick={this.CountIncreaseHandler} className="button btn-green" type="submit"><i className='fa fa-2x fa-plus-circle'></i></button>
+            <button onClick={this.CountResetHandler} className="button btn-red" type="submit"><i className='fa fa-2x fa-refresh'></i></button>
+            <button onClick={this.CountDecreaseHandler.bind(this)} className="button btn-yellow" type="submit"><i className='fa fa-2x fa-minus-circle'></i></button>
+          </div>
+
+        </div>
+
+      </div >
+    );
+
+  }
+
 }
 
 export default App;
